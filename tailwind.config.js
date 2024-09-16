f85f537cf349f9a7a87f61ba1111e4837be8be91
Damián Bastos <damian.bastos@yahoo.com>
2024-09-16 03:45:57 -0300

    portfolio para publicar

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// // tailwind.config.js
+// module.exports = {
+//   content: [
+//     './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}', // Añade los tipos de archivo que necesites
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
